dd5145fec070ebc840c27b68de8a668cad2a5445
Keerthivasan28062002 <keerthivasan.2806@gmail.com>
2024-03-19 16:30:30 +0530

    model evaluation completed

--- src/textSummarizer/conponents/data_transformation.py
+++ src/textSummarizer/conponents/data_transformation.py
@@ -3,7 +3,6 @@ from textSummarizer.logging import logger
 from transformers import AutoTokenizer
 from datasets import load_dataset, load_from_disk
 from textSummarizer.entity import DataTransformationConfig
-import os
 os.environ['CURL_CA_BUNDLE'] = ''
 
 
